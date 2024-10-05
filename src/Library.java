7049ed3e9282fa5a9e28021dbe4abb198985d8b5
Hashim Ali <f223635@cfd.nu.edu.pk>
2024-10-05 14:47:48 +0500

    Added Main class to test Library functions

--- src/Library.java
+++ src/Library.java
@@ -4,12 +4,15 @@ import java.util.List;
 public class Library {
 private List<String> books = new ArrayList();
 
+	// Add Books in the List
 	public void addBook(String bookName) {
 		books.add(bookName);
 		System.out.println(bookName + " added to library.");
 	}
 	
+	// Print List of Books
 	public void listBooks() {
 		System.out.println("Books in library: " + books);
 	} 
 }
+
